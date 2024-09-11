9cdbe55c6cda9a35600f99b339a4e7f15a1b3a26
Arnold Uwamwezi <arnolduwamz7@gmail.com>
2024-09-11 16:42:08 +0300

    Update StoreContext.jsx
    food delivery app

--- front-end/src/context/StoreContext.jsx
+++ front-end/src/context/StoreContext.jsx
@@ -7,7 +7,7 @@ export const StoreContext = createContext(null)
 const StoreContextProvider = (props) => {
      
     const [cartItems, setCartItems] = useState({});
-    const url = 'http://localhost:8000'
+    const url = 'https://food-delivery-back-end-ghtl.onrender.com'
     const [token, setToken] = useState(" ");
     const [food_list, setFoodList] = useState([])
 
