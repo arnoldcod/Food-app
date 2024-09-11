758e66c7eeb7173bad85221a948f5a34b042547b
Arnold Uwamwezi <arnolduwamz7@gmail.com>
2024-09-11 17:05:41 +0300

    Update orderController.js
    food delivery app

--- back-end/controllers/orderController.js
+++ back-end/controllers/orderController.js
@@ -8,7 +8,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 // placing user order from front-end
 const placeOrder = async (req, res) => {
 
-    const frontend_url = "https://food-delivery-front-end-mh4b.onrender.com/"
+    const frontend_url = "https://food-delivery-front-end-mh4b.onrender.com"
 
     try {
         const newOrder = new orderModel({
